2f57747d62315517987f559d775d9d3e459c3f70
almeidarenato <renatoam@ymail.com>
2020-01-21 15:05:31 -0300

    commit inicial

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from "react-native";
+import Routes from "./src/routes";
+
+export default function App() {
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7D4DE7" />
+      <Routes />
+    </>
+  );
+}
---
+++ Readme.md
@@ -0,0 +1,3 @@
+# Dev Radar - ReactNative
+
+Aplicação mobile em React-Native no projeto DevRadar na semana OmniStack 10.
